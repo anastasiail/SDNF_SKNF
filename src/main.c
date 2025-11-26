3b4b91573ed06797c0615aec30d38e09c4c67df8
anastasiail <anastasiailasova0@gmail.com>
2025-11-26 14:27:38 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,6 +1,19 @@
+#include <stdio.h>
 #include "sdnf_sknf.h"
 
 int main() {
-    run_tests();
+    
+    printf("XOR функция (0110):\n");
+    SDNF_SKNF("0110");
+    printf("\n");
+
+    printf("Конъюнкция (0001):\n");
+    SDNF_SKNF("0001");
+    printf("\n");
+
+    printf("3 переменные: (00001111):\n");
+    SDNF_SKNF("00001111");
+    printf("\n");
+
     return 0;
 }
